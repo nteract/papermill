40ed71f5679e2d1b822d5d612477adac91ddb74f
Kyle Kelley <rgbkrk@gmail.com>
2017-07-06 10:26:50 -0700

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Papermill',
+      version='0.0.1',
+      description='Map Reduce for Notebooks',
+      author='nteract contributors',
+      author_email='jupyter@googlegroups.com',
+      url='https://github.com/nteract/papermill',
+      packages=['papermill'],
+     )
