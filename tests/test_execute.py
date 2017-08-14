1cbc80b2dcfff6b53dd7025abf84fd9dbc9385a0
Eric Massey <emassey@netflix.com>
2017-08-14 16:40:59 -0700

    introduce papermill_io object for easier customization of io

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -23,3 +23,22 @@ class TestNotebookHelpers(unittest.TestCase):
         test_nb = read_notebook(nb_test1_executed_fname)
         self.assertEqual(test_nb.node.cells[0].get('source'), u'# Parameters\nmsg = "Hello"\n')
         self.assertEqual(test_nb.parameters, {'msg': 'Hello'})
+
+
+class TestBrokenNotebook(unittest.TestCase):
+
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test(self):
+        path = get_notebook_path('broken.ipynb')
+        result_path = os.path.join(self.test_dir, 'broken.ipynb')
+        execute_notebook(path, result_path)
+        nb = read_notebook(result_path)
+        self.assertEqual(nb.node.cells[0].execution_count, 1)
+        self.assertEqual(nb.node.cells[1].execution_count, 2)
+        self.assertEqual(nb.node.cells[1].outputs[0].output_type, 'error')
+        self.assertEqual(nb.node.cells[2].execution_count, None)
