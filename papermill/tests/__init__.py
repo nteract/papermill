7abef69e58147dbce3b3486bf0e0acaa2e600d21
Aaron Mak Kang Sheng <aaronmakks@gmail.com>
2017-10-08 09:50:54 +0800

    Add test for notebook_dir

--- papermill/tests/__init__.py
+++ papermill/tests/__init__.py
@@ -11,6 +11,10 @@ def get_notebook_path(*args):
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'notebooks', *args)
 
 
+def get_notebook_dir(*args):
+    return os.path.dirname(get_notebook_path(*args))
+
+
 class RedirectOutput(object):
 
     def __init__(self):
