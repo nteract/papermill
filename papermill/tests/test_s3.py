54d87b6e483ef3cf65c08391ffc04917225ba4fb
Carol Willing <carolcode@willingconsulting.com>
2018-01-13 10:58:07 -0800

    add tests for s3

--- papermill/tests/test_s3.py
+++ papermill/tests/test_s3.py
@@ -1,1 +1,22 @@
 import pytest
+
+from ..s3 import Bucket
+
+
+def test_bucket_init():
+    name = 'hello world'
+    test_bucket = Bucket(name, service=None)
+    assert test_bucket.name is name
+
+
+def test_bucket_missing_params():
+    with pytest.raises(TypeError):
+        test_bucket = Bucket(service=None)
+
+    with pytest.raises(TypeError):
+        test_bucket = Bucket()
+
+
+def test_bucket_list():
+
+    pass
