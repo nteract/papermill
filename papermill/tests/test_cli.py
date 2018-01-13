0154314eec8e95f39a3772f6f1b70241aaf0febe
Carol Willing <carolcode@willingconsulting.com>
2018-01-13 12:20:08 -0800

    parameter order ftw

--- papermill/tests/test_cli.py
+++ papermill/tests/test_cli.py
@@ -215,12 +215,12 @@ def test_cli_path():
     @click.command()
     @click.argument('notebook_path')
     @click.argument('output_path')
-    def mock_papermill(notebook_path, output_path):
+    def cli(notebook_path, output_path):
         click.echo('papermill calling %s and %s!' % notebook_path, output_path)
 
     runner = CliRunner()
-    result = runner.invoke(papermill,
-                           ['notebooks/s3/s3_in/s3-simple_notebook.ipynb',
+    result = runner.invoke(cli,
+                           ['papermill', 'notebooks/s3/s3_in/s3-simple_notebook.ipynb',
                             'notebooks/s3/s3_out/output.ipynb'])
     assert result.output
 
