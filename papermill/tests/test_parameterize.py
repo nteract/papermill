8848125475cf52194e183f93f6ea03a316705fa6
Tim Head <betatim@gmail.com>
2018-01-15 18:54:52 +0100

    Make tests select kernel name based on python version

--- papermill/tests/test_parameterize.py
+++ papermill/tests/test_parameterize.py
@@ -1,17 +1,20 @@
+import sys
 import unittest
 
 from ..api import read_notebook
 from ..execute import _parameterize_notebook
 from . import get_notebook_path
 
+PYTHON = 'python2' if sys.version_info[0] == 2 else 'python3'
 
-class TestNotebookHelpers(unittest.TestCase):
+
+class TestNotebookParametrizing(unittest.TestCase):
     def test_preserving_tags(self):
         # test that other tags on the parameter cell are preserved
         test_nb = read_notebook(get_notebook_path("simple_execute.ipynb"))
         test_nb.node.cells[0]['metadata']['tags'].append('some tag')
 
-        _parameterize_notebook(test_nb.node, 'python3', {'msg': 'Hello'})
+        _parameterize_notebook(test_nb.node, PYTHON, {'msg': 'Hello'})
 
         cell_zero = test_nb.node.cells[0]
         self.assertTrue('some tag' in cell_zero.get('metadata').get('tags'))
@@ -22,9 +25,20 @@ class TestNotebookHelpers(unittest.TestCase):
 
     def test_default_parameters_tag(self):
         test_nb = read_notebook(get_notebook_path("simple_execute.ipynb"))
+
+        _parameterize_notebook(test_nb.node, PYTHON, {'msg': 'Hello'})
+
+        cell_zero = test_nb.node.cells[0]
+        self.assertTrue('default parameters'
+                        in cell_zero.get('metadata').get('tags'))
+        self.assertTrue('parameters'
+                        not in cell_zero.get('metadata').get('tags'))
+
+    def test_no_parameters(self):
+        test_nb = read_notebook(get_notebook_path("simple_execute.ipynb"))
         test_nb.node.cells[0]['metadata']['tags'].append('some tag')
 
-        _parameterize_notebook(test_nb.node, 'python3', {'msg': 'Hello'})
+        _parameterize_notebook(test_nb.node, PYTHON, {'msg': 'Hello'})
 
         cell_zero = test_nb.node.cells[0]
         self.assertTrue('default parameters'
