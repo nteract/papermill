87cb00b6dbb806418f9b376c39686fbcad0ebe66
Carol Willing <carolcode@willingconsulting.com>
2018-01-15 09:19:17 -0800

    unicode ftw :(

--- papermill/tests/test_s3.py
+++ papermill/tests/test_s3.py
@@ -3,10 +3,36 @@ import pytest
 from ..s3 import Bucket, Prefix, Key, S3, split
 
 
+@pytest.fixture
+def bucket_no_service():
+    """Returns a bucket instance with no services"""
+    return Bucket('my_test_bucket')
+
+
+@pytest.fixture
+def bucket_with_service():
+    """Returns a bucket instance with a service"""
+    return Bucket('my_sqs_bucket', ['sqs'])
+
+
+@pytest.fixture
+def bucket_sqs():
+    """Returns a bucket instance with a sqs service"""
+    return Bucket('my_sqs_bucket', ['sqs'])
+
+@pytest.fixture
+def bucket_ec2():
+    """Returns a bucket instance with a ec2 service"""
+    return Bucket('my_sqs_bucket', ['ec2'])
+
+@pytest.fixture
+def bucket_multiservice():
+    """Returns a bucket instance with a ec2 service"""
+    return Bucket('my_sqs_bucket', ['ec2', 'sqs'])
+
 def test_bucket_init():
-    name = 'hello world'
-    test_bucket = Bucket(name, service=None)
-    assert test_bucket.name is name
+    assert Bucket('my_test_bucket')
+    assert Bucket('my_sqs_bucket', 'sqs')
 
 
 def test_bucket_defaults():
@@ -27,9 +53,19 @@ def test_bucket_missing_params():
         Bucket()
 
 
-def test_bucket_list():
+def test_bucket_list(bucket_sqs):
+    # prefix_test = ''
+    # assert bucket_sqs.list(prefix_test)
+    #
+    # prefix_test = 'abc'
+    # assert bucket_sqs.list(prefix_test) is None
+    #
+    # prefix_test = 'ec2'
+    # assert bucket_sqs.list(prefix_test) is None
+    #
+    # prefix_test = 'sqs'
+    # assert bucket_sqs.list(prefix_test)
     pass
-
 
 def test_prefix_init():
     pass
@@ -45,15 +81,13 @@ def test_prefix_defaults():
     assert p1.service == p2.service
 
 
-def test_prefix_str():
-    bucket = Bucket('mydatapool')
-    p1 = Prefix(bucket, 'bigdata')
-    assert str(p1) == 's3://' + str(bucket) + '/bigdata'
+def test_prefix_str(bucket_sqs):
+    p1 = Prefix(bucket_sqs, 'sqs_prefix_test', 'sqs')
+    assert str(p1) == 's3://' + str(bucket_sqs) + '/sqs_prefix_test'
 
 
-def test_prefix_repr():
-    bucket = Bucket('mydatapool')
-    p1 = Prefix(bucket, 'bigdata')
+def test_prefix_repr(bucket_sqs):
+    p1 = Prefix(bucket_sqs, 'sqs_prefix_test', 'sqs')
     assert p1.__repr__
 
 
@@ -80,6 +114,7 @@ def test_s3_defaults():
     assert s1.session == s2.session
     assert s1.client == s2.client
     assert s1.s3 == s2.s3
+
 
 @pytest.mark.parametrize("value,expected", [
     ('s3://foo/bar/baz', ['foo', 'bar/baz']),
