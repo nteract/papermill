09c2240a699e1ecfca22e831c2a4959ca14ff231
Carol Willing <carolcode@willingconsulting.com>
2018-01-15 11:15:53 -0800

    add options to theme sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,9 @@ html_static_path = ['_static']
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
     '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
+        'about.html',
+        'navigation.html',
+        'relations.html',
         'searchbox.html',
     ]
 }
