68a507951fc815b10795226e877a6d0196ee97aa
Harsha Mandai <Harsha_Mandadi@intuit.com>
2018-09-15 20:35:54 +0530

    testing with pytest as python 2.7 is giving errors

--- papermill/tests/test_iorw.py
+++ papermill/tests/test_iorw.py
@@ -5,7 +5,7 @@ import unittest
 import os
 import io
 import tempfile
-import warnings
+import pytest
 from requests.exceptions import ConnectionError
 
 try:
@@ -80,11 +80,8 @@ class TestPapermillIO(unittest.TestCase):
         )
 
     def test_read_with_no_file_extension(self):
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
-            # Trigger a warning.
+        with pytest.warns(UserWarning):
             self.papermill_io.read("fake/path")
-            assert "the file is not specified with any format" in str(w[-1].message)
 
     def test_listdir(self):
         self.assertEqual(self.papermill_io.listdir("fake/path"), ["fake", "contents"])
@@ -93,11 +90,8 @@ class TestPapermillIO(unittest.TestCase):
         self.assertEqual(self.papermill_io.write("buffer", "fake/path"), "wrote buffer")
 
     def test_write_with_no_file_extension(self):
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
-            # Trigger a warning.
+        with pytest.warns(UserWarning):
             self.papermill_io.write("buffer", "fake/path")
-            assert "the file is not specified with any format" in str(w[-1].message)
 
     def test_pretty_path(self):
         self.assertEqual(self.papermill_io.pretty_path("fake/path"), "fake/path/pretty/1")
