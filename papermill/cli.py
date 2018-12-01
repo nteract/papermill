e3d7e8e4f3227db9b928dc22a1b36622f7e2ecfc
Christopher Bacher <mindsbackyard@gmail.com>
2018-12-01 12:31:26 +0100

    fix lint errors

--- papermill/cli.py
+++ papermill/cli.py
@@ -49,19 +49,22 @@ def print_papermill_version(ctx, param, value):
     '--inject-input-path',
     is_flag=True,
     default=False,
-    help="Insert the path of the input notebook as PAPERMILL_INPUT_PATH in the notebook's parameters.",
+    help="Insert the path of the input notebook as PAPERMILL_INPUT_PATH as a notebook parameter.",
 )
 @click.option(
     '--inject-output-path',
     is_flag=True,
     default=False,
-    help="Insert the path of the output notebook as PAPERMILL_OUTPUT_PATH in the notebook's parameters.",
+    help="Insert the path of the output notebook as PAPERMILL_OUTPUT_PATH as a notebook parameter.",
 )
 @click.option(
     '--inject-paths',
     is_flag=True,
     default=False,
-    help="Insert the paths of both input/output notebook as PAPERMILL_INPUT_PATH/PAPERMILL_OUTPUT_PATH in the notebook's parameters.",
+    help=(
+        "Insert the paths of input/output notebooks as PAPERMILL_INPUT_PATH/PAPERMILL_OUTPUT_PATH"
+        " as notebook parameters."
+    ),
 )
 @click.option('--engine', help='The execution engine name to use in evaluating the notebook.')
 @click.option(
