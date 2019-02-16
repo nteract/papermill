bf7487b2064a4e54e02123d24a8036e44e752d77
Gonzalo Gasca Meza <gogasca@google.com>
2019-02-16 10:26:51 -0800

    Modify tests after Code review

--- papermill/tests/test_gcs.py
+++ papermill/tests/test_gcs.py
@@ -1,75 +1,103 @@
+import six
+import unittest
+from retry.api import retry_call
+from gcsfs.utils import HtmlError as RateLimitException
 from ..iorw import GCSHandler
 
+if six.PY3:
+  from unittest.mock import patch
+else:
+  from mock import patch
 
-class RetryableError(Exception):
-    pass
+_RETRIES = 3
 
 
 class MockGCSFileSystem(object):
-    def __init__(self):
-        self._file = MockGCSFile()
+  def __init__(self):
+    self._file = MockGCSFile()
 
-    def open(self, *args, **kwargs):
-        return self._file
+  def open(self, *args, **kwargs):
+    return self._file
 
-    def ls(self, *args, **kwargs):
-        return []
+  def ls(self, *args, **kwargs):
+    return []
 
 
 class MockGCSFile(object):
-    def __enter__(self):
-        self._value = 'default value'
-        return self
 
-    def __exit__(self, *args, **kwargs):
-        pass
+  def __init__(self):
+    self.write_count = 0
 
-    def read(self):
-        return self._value
+  def __enter__(self):
+    self._value = 'default value'
+    return self
 
-    def write(self, buf):
-        if not buf:
-            raise RetryableError
+  def __exit__(self, *args, **kwargs):
+    pass
+
+  def read(self):
+    return self._value
+
+  def write(self, buf):
+    self.write_count += 1
+    if buf == 'raise_limit_exception':
+      raise RateLimitException
+    elif buf == 'retry':
+      if self.write_count < _RETRIES:
+        raise RateLimitException
+      else:
+        return self.write_count
+    else:
+      pass
 
 
-def test_gcs_read(mocker):
-    mocker.patch('papermill.iorw.GCSFileSystem', MockGCSFileSystem)
-    gcs_handler = GCSHandler()
-    client = gcs_handler._get_client()
-    assert gcs_handler.read('gs://bucket/test.ipynb') == 'default value'
+class GCSTest(unittest.TestCase):
+  """
+     Tests for `GCS`
+  """
 
+  def setUp(self):
+    self.gcs_handler = GCSHandler()
+
+  @patch('papermill.iorw.GCSFileSystem', side_effect=MockGCSFileSystem)
+  def test_gcs_read(self, mock_gcs_filesystem):
+    client = self.gcs_handler._get_client()
+    assert self.gcs_handler.read('gs://bucket/test.ipynb') == 'default value'
     # Check that client is only generated once
-    assert client is gcs_handler._get_client()
+    self.assertIs(client, self.gcs_handler._get_client())
 
-
-def test_gcs_write(mocker):
-    mocker.patch('papermill.iorw.GCSFileSystem', MockGCSFileSystem)
-    gcs_handler = GCSHandler()
-    client = gcs_handler._get_client()
-    gcs_handler.write('new value', 'gs://bucket/test.ipynb')
+  @patch('papermill.iorw.GCSFileSystem', side_effect=MockGCSFileSystem)
+  def test_gcs_write(self, mock_gcs_filesystem):
+    client = self.gcs_handler._get_client()
+    self.gcs_handler.write('new value', 'gs://bucket/test.ipynb')
     # Check that client is only generated once
-    assert client is gcs_handler._get_client()
+    self.assertIs(client, self.gcs_handler._get_client())
 
-
-def test_gcs_write_retry(mocker):
-    mocker.patch('papermill.iorw.GCSFileSystem', MockGCSFileSystem)
-    counter = 0
-    try:
-        gcs_handler = GCSHandler()
-        client = gcs_handler._get_client()
-        gcs_handler.write('', 'gs://bucket/test.ipynb')
-    except RetryableError:
-        counter += 1
-    assert counter == 1
+  @patch('papermill.iorw.GCSFileSystem', side_effect=MockGCSFileSystem)
+  def test_gcs_listdir(self, mock_gcs_filesystem):
+    client = self.gcs_handler._get_client()
+    self.gcs_handler.listdir('testdir')
     # Check that client is only generated once
-    assert client is gcs_handler._get_client()
+    self.assertIs(client, self.gcs_handler._get_client())
 
+  @patch('papermill.iorw.GCSFileSystem', side_effect=MockGCSFileSystem)
+  def test_gcs_handle_exception(self, mock_gcs_filesystem):
+    with self.assertRaises(RateLimitException):
+      retry_call(self.gcs_handler.write,
+                 exceptions=RateLimitException,
+                 fargs=['raise_limit_exception', 'gs://bucket/test.ipynb'],
+                 tries=_RETRIES,
+                 delay=1,
+                 backoff=1,
+                 max_delay=2)
 
-def test_gcs_listdir(mocker):
-    mocker.patch('papermill.iorw.GCSFileSystem', MockGCSFileSystem)
-    gcs_handler = GCSHandler()
-    client = gcs_handler._get_client()
-    gcs_handler.listdir('testdir')
-
-    # Check that client is only generated once
-    assert client is gcs_handler._get_client()
+  @patch('papermill.iorw.GCSFileSystem', side_effect=MockGCSFileSystem)
+  def test_gcs_retry(self, mock_gcs_filesystem):
+    write_count = retry_call(self.gcs_handler.write,
+                             exceptions=RateLimitException,
+                             fargs=['retry', 'gs://bucket/test.ipynb'],
+                             tries=_RETRIES,
+                             delay=1,
+                             backoff=1,
+                             max_delay=2)
+    self.assertEqual(write_count, _RETRIES)
