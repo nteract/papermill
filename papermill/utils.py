fd9cadfa77dd97eca8a559fdc689f3b4d9143daa
vizhur <vizhur@live.com>
2019-05-08 08:50:48 -0400

    adding utils to handle kwargs

--- papermill/utils.py
+++ papermill/utils.py
@@ -6,6 +6,25 @@ from functools import wraps
 
 logger = logging.getLogger('papermill.utils')
 
+WARN_ON_ARGS_OVERWRITE = True
+
+def safe_kwargs(caller_args, **callee_args):
+    if WARN_ON_ARGS_OVERWRITE:
+        conflicts = set(caller_args) & set(callee_args)
+        if conflicts:
+            args = format('; '.join(['{}={}'.format(key, value)
+                                     for key, value in callee_args.items()]))
+            msg = 'Callee will overwrite caller''s argument(s): {}'.format(args)
+            warnings.warn(msg)
+    return dict(caller_args, **callee_args)
+
+
+def remove_args(args=None, kwargs=None):
+    kwargs = kwargs if kwargs else {}
+    if not args:
+        return kwargs
+    return dict([(key, kwargs.get(key)) for key in kwargs if key not in args])
+
 
 # retry decorator
 def retry(num):
