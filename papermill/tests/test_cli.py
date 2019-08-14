61c65b0842aa9ce06db47278c0b89d6a31fb6da2
gshiba <gosuke@borealgenomics.com>
2019-08-13 17:39:20 -0700

    expand test

--- papermill/tests/test_cli.py
+++ papermill/tests/test_cli.py
@@ -105,12 +105,14 @@ class TestCLI(unittest.TestCase):
     @patch(cli.__name__ + '.execute_notebook')
     def test_parameters_list_args(self, execute_patch):
         self.runner.invoke(
-            papermill, self.default_args + ['-p', 'foo', 'bar', 'baz', '--parameters', 'baz', '42']
+            papermill, self.default_args + ['-p', 'foo', 'bar', 'baz', '0',
+                                            '-p', 'one', 'two',
+                                            '--parameters', 'qux', '42']
         )
         execute_patch.assert_called_with(
             'input.ipynb',
             'output.ipynb',
-            {'foo': ['bar', 'baz'], 'baz': 42},
+            {'foo': ['bar', 'baz', 0], 'one': 'two', 'qux': 42},
             engine_name=None,
             request_save_on_cell_execute=True,
             prepare_only=False,
