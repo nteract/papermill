06feedddb95b0c9bc7e0b491eff9390c30c131d4
Tyler Calder <calder-ty@protonmail.com>
2021-08-30 11:35:27 -0600

    Add tests for showing unpickling error

--- papermill/tests/test_exceptions.py
+++ papermill/tests/test_exceptions.py
@@ -1,1 +1,26 @@
+import os
+import pickle
+import tempfile
+
 import pytest  # noqa
+
+from .. import exceptions
+
+
+@pytest.fixture
+def temp_file():
+    f = tempfile.NamedTemporaryFile()
+    yield f
+    f.close()
+
+
+def test_exceptions_are_unpickleable(temp_file):
+    """Ensure Exceptions can be unpickled"""
+    err = exceptions.PapermillExecutionError(1, 2, "TestSource", "Exception", Exception(), ["Traceback", "Message"])
+    with open(temp_file.name, 'wb') as fd:
+        pickle.dump(err, fd)
+    # Read File
+    temp_file.seek(0)
+    data = temp_file.read()
+    pickled_err = pickle.loads(data)
+    assert str(pickled_err) == str(err)
