e304481d8b122e2e74698526105ff3c039eea789
Ben Beasley <code@musicinmybrain.net>
2023-11-01 17:54:43 -0400

    Add “github” extra deps. to the “all” extra (#715)
    Co-authored-by: Matthew Seal <mseal007@gmail.com>

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ hdfs_reqs = read_reqs('hdfs.txt', folder='requirements')
 github_reqs = read_reqs('github.txt', folder='requirements')
 docs_only_reqs = read_reqs('requirements.txt', folder='docs')
 black_reqs = ['black >= 19.3b0']
-all_reqs = s3_reqs + azure_reqs + gcs_reqs + hdfs_reqs + black_reqs
+all_reqs = s3_reqs + azure_reqs + gcs_reqs + hdfs_reqs + github_reqs + black_reqs
 docs_reqs = all_reqs + docs_only_reqs
 # Temporarily remove hdfs_reqs from dev deps until the pyarrow package is available for Python 3.12
 dev_reqs = read_reqs('dev.txt', folder='requirements') + s3_reqs + azure_reqs + gcs_reqs + black_reqs # all_reqs
