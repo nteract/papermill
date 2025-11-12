2fdbca7c1248dcd0cbfc343d086307f9ba7744b8
Carol Willing <carolcode@willingconsulting.com>
2025-11-12 09:57:57 -0800

    Drop Python 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -96,8 +96,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
