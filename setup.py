8677f67c66f2a68d32df0d91952a7835ec20e294
Carol Willing <carolcode@willingconsulting.com>
2025-11-12 17:40:09 -0800

    Remove 3.14

--- setup.py
+++ setup.py
@@ -100,6 +100,5 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
     ],
 )
